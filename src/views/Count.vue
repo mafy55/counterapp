969987d4e570d182179eea080a315d2b0b7745b0
3str <3streamer@gmail.com>
2020-06-22 04:38:25 +0900

    カウント+Vuexの導入

---
+++ src/views/Count.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+<p>{{ count }}</p>
+  <p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            count:'0'
+        }
+    },
+    methods:{
+        increment(){
+            this.count++
+        },
+        decrement(){
+            this.count--
+        }
+    }
+}
+</script>
---
+++ src/views/Score.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="page">
+<h1>スコア表示</h1>
+<p>{{ score }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            
+        }
+    },
+    computed:{
+        score(){
+            return this.$store.getters.count
+        }
+
+    }
+}
+</script>
